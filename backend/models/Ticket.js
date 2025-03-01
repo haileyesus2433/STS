a9434ea5efd8c353b566101f9e65efc55f19a94b
haileyesus <haileyesusbe@gmail.com>
2025-03-01 13:43:47 -0500

    feat: setup tickets schema

---
+++ backend/models/Ticket.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const TicketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['open', 'in progress', 'closed'],
+    default: 'open'
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Ticket = mongoose.model('Ticket', TicketSchema);
+
+export default Ticket;
